2639ef2fc9d537f8d76934c877f0990086cb2a6a
simque <simon.duncanson@gmail.com>
2016-01-11 12:38:46 -0500

    Done.

--- lib/play.rb
+++ lib/play.rb
@@ -1,5 +1,6 @@
 # Helper Methods
 def display_board(board)
+  system 'clear'
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
@@ -19,22 +20,59 @@ def valid_move?(board, position)
   position.to_i.between?(1,9) && !position_taken?(board, position.to_i-1)
 end
 
-def turn(board)
-  puts "Please enter 1-9:"
+def computer_makes_choice(board)
+  empty_squares = []
+  board.each_with_index do |marker, idx|
+    empty_squares << idx if marker == " "
+  end
+  empty_squares
+end
+
+def joinor(array)
+  if array.length > 1
+    array[-1] = "or #{array[-1]}"
+  else
+    array
+  end
+  array.join(", ")
+end
+
+def valid_spaces(board)
+  computer_makes_choice(board).map{|e| e + 1}
+end
+
+
+def player_turn(board)
+  puts "Please enter #{joinor(valid_spaces(board))}:"
   input = gets.strip
   if valid_move?(board, input)
     move(board, input)
   else
-    turn(board)
+    player_turn(board)
   end
+  display_board(board)
+end
+
+def computer_turn(board)
+  computer_choice = computer_makes_choice(board).sample
+  board[computer_choice] = "O"
   display_board(board)
 end
 
 # Define your play method below
 def play(board)
   number_of_turns = 0
-  until number_of_turns == 9
+  loop do
+    player_turn(board)
     number_of_turns += 1
-    turn(board)
+    break if number_of_turns == 9
+    computer_turn(board)
+    number_of_turns += 1
   end
 end
+
+# revisiting this lab - added to code to (1) include the computer
+# as a second player (2) use string interpolation and a custom #joinor
+# method to advise player of the remaining spaces that are available
+# (3) make use of a loop to continue game play until 9 turns have been taken
+# (4) a goodbye message at the end of the game
