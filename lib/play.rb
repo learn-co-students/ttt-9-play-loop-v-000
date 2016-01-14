28b6c036d818535662d649585fc43c63dbe017ac
thomjonson <thomjonson@aol.com>
2016-01-13 23:47:27 -0500

    Done.

--- lib/play.rb
+++ lib/play.rb
@@ -30,4 +30,14 @@ def turn(board)
   display_board(board)
 end
 
-# Define your play method below
+def play(board)
+  counter = 0
+  loop do
+    counter += 1
+    turn(board)
+    if counter == 9
+      break
+    end
+    
+  end
+end
