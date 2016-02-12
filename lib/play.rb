2b56d6f451e88b2a51d14ef39a970860bc2e1da2
asherfsullivan <asherfolsomsullivan@gmail.com>
2016-02-12 15:36:02 -0500

    Done.

--- lib/play.rb
+++ lib/play.rb
@@ -24,10 +24,16 @@ def turn(board)
   input = gets.strip
   if valid_move?(board, input)
     move(board, input)
+    display_board(board)
   else
     turn(board)
   end
-  display_board(board)
 end
 
-# Define your play method below
+def play(board)
+  turns = 0
+  until turns == 9
+    turn(board)
+    turns +=1
+  end
+end
