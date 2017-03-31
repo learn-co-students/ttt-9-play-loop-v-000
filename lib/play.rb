9579fde222e473f4438a64703f54820bd36728b6
Susan Fowler <infopro94@gmail.com>
2017-03-31 21:44:34 +0000

    Done.

--- lib/play.rb
+++ lib/play.rb
@@ -35,4 +35,10 @@ def turn(board)
   end
 end
 
-# Define your play method below
+def play(board)
+  turn(board)
+  until
+    board == (["X","X","X","X","X","X","X","X","X"])
+    play(board)
+end
+  end
