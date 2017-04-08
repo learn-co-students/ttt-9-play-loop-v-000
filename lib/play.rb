fcaae1b5ff7f4c7362573c9db99f02322750d6bf
Wen Yu <wyu10amy@hotmail.com>
2017-04-08 04:01:41 +0000

    Done.

--- lib/play.rb
+++ lib/play.rb
@@ -1,4 +1,3 @@
-# Helper Methods
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -7,20 +6,33 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def input_to_index(user_input)
-  user_input.to_i - 1
-end
-
-def move(board, index, current_player = "X")
-  board[index] = current_player
-end
-
-def position_taken?(board, location)
-  board[location] != " " && board[location] != ""
-end
-
 def valid_move?(board, index)
-  index.between?(0,8) && !position_taken?(board, index)
+  if index.to_i.between?(0,8)
+    if board[index] == " " || board[index] == ""|| board[index] = nil
+      true
+    elsif
+      false
+    end
+  end
+end
+
+
+
+def input_to_index(input)
+  index = input.to_i - 1
+end
+
+def move(board, index, token="X")
+  board[index] = token
+  board
+end
+
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    false
+  elsif
+    true
+  end
 end
 
 def turn(board)
@@ -28,11 +40,21 @@ def turn(board)
   input = gets.strip
   index = input_to_index(input)
   if valid_move?(board, index)
-    move(board, index)
-    display_board(board)
-  else
+    move(board, index, token="X")
+    puts token # testing, still work without this line
+  elsif
     turn(board)
   end
+  display_board(board)
 end
 
+
 # Define your play method below
+
+def play(board)
+    int = 0
+  while int < 9 do
+    turn(board)
+    int += 1
+  end
+end
