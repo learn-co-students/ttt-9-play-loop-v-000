7c4910448ce8354743653ce7d7ba0ea0d4834a4d
Juliana baggio <baggiojuliana@gmail.com>
2017-04-14 19:03:57 +0000

    Done.

--- lib/play.rb
+++ lib/play.rb
@@ -38,10 +38,9 @@ end
 # Define your play method below
 
 def play(board)
- counter = 0
- loop do
+ turn_count = 0
+ while turn_count < 9
  turn(board)
- counter += 1
- break if counter == 9
+ turn_count += 1
  end
  end
