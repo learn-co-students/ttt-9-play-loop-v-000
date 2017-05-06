0e12c29a712760f2a228a07b6afa217ea910cb85
Erin Doyle <erin.v.doyle@gmail.com>
2017-05-06 14:32:53 +0000

    Done.

--- lib/play.rb
+++ lib/play.rb
@@ -37,9 +37,9 @@ end
 
 # Define your play method below
 def play(board)
-  counter = 0
-  until counter == 9
+  play = 0
+  until play == 9
     turn(board)
-    counter += 1
+    play += 1
   end
 end
