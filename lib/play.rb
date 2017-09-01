e6b52a35890142245734b42cc2a472627adc0973
Allen Anderson <dande313@byu.net>
2017-09-01 17:08:28 +0000

    Done.

--- lib/play.rb
+++ lib/play.rb
@@ -37,9 +37,9 @@ end
 
 # Define your play method below
 def play(board)
-  turn_count = 1
-  until turn_count > 9 do
+  counter = 0
+  until counter == 9 do
     turn(board)
-    turn_count+=1
+    counter+=1
   end
 end
