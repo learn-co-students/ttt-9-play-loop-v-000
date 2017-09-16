00f76e14a801f71737d7cf5fec44185d041c96eb
Jessica Mott <wigmore3.14@gmail.com>
2017-09-16 17:47:25 +0000

    Done.

--- lib/play.rb
+++ lib/play.rb
@@ -1,4 +1,3 @@
-# Helper Methods
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -35,4 +34,10 @@ def turn(board)
   end
 end
 
-# Define your play method below
+def play(board)
+  turn = 0
+  while turn < 9
+    turn(board)
+    turn +=1
+  end
+end
