c3f497d08a30e5957f24587c399b1d328b1df13a
Ssenkowski <30485389+Ssenkowski@users.noreply.github.com>
2017-10-21 08:05:10 -0400

    Update play.rb

--- lib/play.rb
+++ lib/play.rb
@@ -16,7 +16,7 @@ def move(board, index, current_player = "X")
 end
 
 def position_taken?(board, location)
-  board[location] != " " && board[location] != ""
+  board[location] != " " 
 end
 
 def valid_move?(board, index)
