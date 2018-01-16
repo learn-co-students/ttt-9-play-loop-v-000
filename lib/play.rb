03f67fb2c09f6130426320f300ae0e3925634fcd
Devin Wilson <craigvin84@yahoo.com>
2018-01-16 02:32:24 +0000

    Done.

--- lib/play.rb
+++ lib/play.rb
@@ -7,6 +7,14 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+def display_board_full(board)
+  puts " #{"X"||"O"} | #{"X"||"O"} | #{"X"||"O"} "
+  puts "-----------"
+  puts " #{"X"||"O"} | #{"X"||"O"} | #{"X"||"O"} "
+  puts "-----------"
+  puts " #{"X"||"O"} | #{"X"||"O"} | #{"X"||"O"} "
+end
+
 def input_to_index(user_input)
   user_input.to_i - 1
 end
@@ -35,4 +43,9 @@ def turn(board)
   end
 end
 
-# Define your play method below
+def play(board)
+  turn(board)
+  until board == ["X"||"O", "X"||"O", "X"||"O", "X"||"O", "X"||"O", "X"||"O", "X"||"O", "X"||"O", "X"||"O"]
+    turn(board)
+  end
+end
