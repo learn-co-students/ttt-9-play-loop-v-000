661f8ebc014c75d6e19440575c2391b455a41eef
Karl Amoguis <ktamoguis@gmail.com>
2018-04-19 15:02:11 +0000

    Done.

--- lib/play.rb
+++ lib/play.rb
@@ -1,3 +1,14 @@
+
+def play(board)
+  turns = 0
+  until turns == 9
+    turn(board)
+    turns +=1
+  end
+end
+
+
+
 # Helper Methods
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
