202fd35eb00ef5a565b8b411a2243b764b99f25c
CHRISTOPHER WOOD <cjwood032@gmail.com>
2018-05-11 01:27:20 +0000

    Done.

--- lib/play.rb
+++ lib/play.rb
@@ -1,3 +1,4 @@
+
 # Helper Methods
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
@@ -36,3 +37,10 @@ def turn(board)
 end
 
 # Define your play method below
+def play(board)
+  turncount = 0
+  while turncount<9
+    turn(board)
+    turncount+=1
+  end
+end
