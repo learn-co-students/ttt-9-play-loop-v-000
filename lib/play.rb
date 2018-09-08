6fb01af0fd3557a78914b58726667e52d6ee98a3
Rose Brennan <brennan.m.rose@gmail.com>
2018-09-08 15:02:20 +0000

    Done.

--- lib/play.rb
+++ lib/play.rb
@@ -36,11 +36,18 @@ def turn(board)
 end
 
 def play(board)
-  turn = 0
-  
+    turn = 0
     until turn == 9
     turn(board)
     turn += 1
   end
 end
+
+#def play(board)
+  #turn_count = 0
+  #while turn_count < 9
+    #turn(board)
+    #turn_count+=1
+  #end
+#end
 
