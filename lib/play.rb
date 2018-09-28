ebe6fecc15518805af7abddd2405384a07befa0a
kitte anderson <kitteanderson@gmail.com>
2018-09-28 17:17:19 +0000

    Done.

--- lib/play.rb
+++ lib/play.rb
@@ -37,9 +37,9 @@ end
 
 # Define your play method below
 def play(board)
-  i = 0
-  while i < 9
-      i += 1
-      turn(board)
-    end
+    input = 0
+until input == 9
+    turn(board)
+    input += 1
   end
+end
