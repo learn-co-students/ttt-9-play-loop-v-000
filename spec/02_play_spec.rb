44dfd9784d0027b121870d340a75250179c1f2b2
CharlesOK <okwechimeamechi@gmail.com>
2020-09-28 18:49:58 -0400

    Done.

--- spec/02_play_spec.rb
+++ spec/02_play_spec.rb
@@ -2,12 +2,12 @@ require_relative '../lib/play'
 
 describe '#play' do
   it 'calls turn nine times' do
-    board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+    board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
 
-    allow(self).to receive(:gets).and_return('1','2','3','4','5','6','7','8','9')
-    
+    allow(self).to receive(:gets).and_return('1', '2', '3', '4', '5', '6', '7', '8', '9')
+
     play(board)
 
-    expect(board).to eq(["X","X","X","X","X","X","X","X","X",])
+    expect(board).to eq(%w[X X X X X X X X X])
   end
 end
